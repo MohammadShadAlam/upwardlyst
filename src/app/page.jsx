c5b8df25f4c111b658aabd395f3ebab2c03f03eb
MohammadShadAlam <mohammadshad054@gmail.com>
2026-07-27 00:39:40 +0530

    Updated name

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import HomePage from "../Page/Home";
 
 export const metadata = {
-    title: "Home - Marko - Digital Marketing Agency",
+    title: "Upwardlyst | Digital Marketing Agency",
 };
 
 export default function Page() {
